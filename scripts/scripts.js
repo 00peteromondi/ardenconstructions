239d8280426d1220daf7a0ccc0948e633e647f7e
Peter Omondi <00peteromondi@gmail.com>
2025-03-04 21:10:57 +0300

    fixed navbar toggler

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
+   
     const navbarLinks = document.querySelectorAll('.navbar a');
 
     function onScroll() {
         const scrollPos = window.scrollY || document.documentElement.scrollTop;
         navbarLinks.forEach(link => {
             const section = document.querySelector(link.hash);
-            const offset = 120; // Adjust this value for the desired padding
+            const offset = 100; // Adjust this value for the desired padding
             if (section.offsetTop - offset <= scrollPos && section.offsetTop + section.offsetHeight - offset > scrollPos) {
                 link.classList.add('active');
             } else {
@@ -25,18 +26,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
     window.addEventListener('scroll', onScroll);
 
-    // Call onScroll initially to set the active class based on the initial scroll position
     onScroll();
-    // Adjust scroll position to avoid hiding content behind the navbar
-    const hash = window.location.hash;
-    if (hash) {
-        const target = document.querySelector(hash);
-        if (target) {
-            const offset = 150; // Same offset value as in onScroll function
-            window.scrollTo({
-                top: target.offsetTop - offset,
-                behavior: 'smooth'
-            });
-        }
-    }
+   
 });
